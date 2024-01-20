52e19a2b827b79e328eb39b9d47ffc206fe742a8
F <puppy02468@gmail.com>
2024-01-19 23:00:15 -0500

    updating home y2k aesthetic

---
+++ script.js
@@ -0,0 +1,6 @@
+// Show the popup on page load
+document.addEventListener("DOMContentLoaded", showPopup);
+
+function showPopup() {
+    document.getElementById("y2kPopup").style.display = "flex";
+}
