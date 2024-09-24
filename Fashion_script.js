ebc223fd0d85fe52465c5da14070f57915447671
FaybenM <60396543+FaybenM@users.noreply.github.com>
2024-09-24 04:08:32 -0400

    fix butterfly cursor

--- Fashion_script.js
+++ Fashion_script.js
@@ -1,6 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+  
 
 document.addEventListener('mousemove', (event) => {
   const cursorTrail = document.getElementById('cursor-trail');
   cursorTrail.style.left = `${event.pageX}px`;
   cursorTrail.style.top = `${event.pageY}px`;
+});
+
 });
