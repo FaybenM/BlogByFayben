527e07558c4e98188091b45e0dccc28bbb45c2d7
FaybenM <60396543+FaybenM@users.noreply.github.com>
2024-09-24 04:24:38 -0400

    update css for fashion

--- Fashion_script.js
+++ Fashion_script.js
@@ -9,12 +9,3 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 
-is this code needed in my css:  
-.butterfly {
-  position: absolute;
-  pointer-events: none;
-  font-size: 1.5rem; /* Adjust size of the butterfly emoji */
-  transform: translate(-50%, -50%);
-  transition: transform 0.3s, opacity 0.3s;
-  opacity: 1;
-}
