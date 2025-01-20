dda473ed0e5c6da7de9105f4ee6bd7cd6aae6593
FaybenM <60396543+FaybenM@users.noreply.github.com>
2025-01-20 05:07:26 -0500

    change colors to 3 for dust

--- Fashion_script.js
+++ Fashion_script.js
@@ -1,6 +1,6 @@
 window.addEventListener("load", (event) => {
     new cursoreffects.fairyDustCursor();
-    colors: ["#d2cced", "#aea8d3", "#8b85ba", "#6a64a1", "#4b4388", "#30216f"],
+    colors: ["#d2cced", "#6a64a1", "#30216f"],
   });
 
  
