fdc36f8de1ee4ccdda1c3185d34fd7932dc552fe
FaybenM <60396543+FaybenM@users.noreply.github.com>
2025-01-20 05:16:05 -0500

    colors again

--- Fashion_script.js
+++ Fashion_script.js
@@ -1,6 +1,6 @@
 window.addEventListener("load", (event) => {
     new cursoreffects.fairyDustCursor({
-      colors: ["#D61C59", "#E7D84B", "#1B8798"],
+      colors: ["#D61C59", "#1B8798", "#E7D84B"],
     });
   });
   
