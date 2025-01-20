59b5c79c018bdd52581ef026969a7bbd3bc09a05
FaybenM <60396543+FaybenM@users.noreply.github.com>
2025-01-20 05:04:34 -0500

    fairy trail colors

--- Fashion_script.js
+++ Fashion_script.js
@@ -1,3 +1,7 @@
 window.addEventListener("load", (event) => {
     new cursoreffects.fairyDustCursor();
   });
+
+  new cursoreffects.fairyDustCursor({
+    colors: ["#d2cced", "#aea8d3", "#8b85ba", "#6a64a1", "#4b4388", "#30216f"],
+  });
