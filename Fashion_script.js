68f2c3fa72c308947a3c1e7af46bce0a46f172b3
FaybenM <60396543+FaybenM@users.noreply.github.com>
2025-01-20 05:20:17 -0500

    change to emojis

--- Fashion_script.js
+++ Fashion_script.js
@@ -1,6 +1,6 @@
 window.addEventListener("load", (event) => {
-    new cursoreffects.fairyDustCursor({
-      colors: ["#D61C59", "#1B8798", "#E7D84B"],
+    new cursoreffects.emojiCursor({
+        new cursoreffects.emojiCursor({ emoji: ["🩷", "🩵", "💜"] });
     });
   });
   
